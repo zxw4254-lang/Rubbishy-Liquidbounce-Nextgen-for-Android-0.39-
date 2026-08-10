65d6073a460c2aa4fe350ece12c26db66ebabc43
zxw4254-lang <zxw4254@gmail.com>
2026-08-10 13:27:49 +0800

    Update ModuleManager.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -214,6 +214,7 @@ import net.ccbluex.liquidbounce.features.module.modules.render.ModuleMobOwners
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNewChunks
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoBob
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoFov
+import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNotifications
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoHurtCam
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleNoSwing
 import net.ccbluex.liquidbounce.features.module.modules.render.ModuleParticles
